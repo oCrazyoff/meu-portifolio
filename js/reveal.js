61b59ef39c2aeb136393810c9dbb2090a20999fa
oCrazyoff <walyssonribeiro3@gmail.com>
2024-08-20 10:06:16 -0300

    add scroll reveal

---
+++ js/reveal.js
@@ -0,0 +1,12 @@
+window.revelar = ScrollReveal({ reset: true })
+
+revelar.reveal('.efeito', {
+    duration: 2000,
+    distance: '30px'
+})
+
+revelar.reveal('.avatar.efeito', {
+    duration: 2000,
+    distance: '30px',
+    delay: 500
+})
