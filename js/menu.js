acae47f9a8845541b83a58f511f0b8b5dd681291
oCrazyoff <walyssonribeiro3@gmail.com>
2024-08-21 16:36:23 -0300

    update menu

--- js/menu.js
+++ js/menu.js
@@ -9,19 +9,3 @@ btn.addEventListener('click', () => {
 item.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
-
-window.addEventListener('resize', function() {
-    const listaItens = document.querySelectorAll('ul li');
-    
-    if (window.innerWidth <= 1020) {
-        listaItens.forEach(function(li) {
-            li.classList.remove('efeito');
-        });
-    } else {
-        listaItens.forEach(function(li) {
-            li.classList.add('efeito');
-        });
-    }
-});
-
-window.dispatchEvent(new Event('resize'));
